7716ce3a75af21284af285d9fb2edae692aad811
Chriszzz <chrisz2zz@duck.com>
2025-03-10 01:00:39 +0800

    feat: 20250309

--- dp/22-maxProfit2/main.go
+++ dp/22-maxProfit2/main.go
@@ -16,8 +16,8 @@ func maxProfit(prices []int) int {
 	dp[0][0] = -prices[0]
 
 	for i := 1; i < len(prices); i++ {
-		dp[i][0] = max(dp[i-1][0], -prices[i])
-		dp[i][1] = max(dp[i-1][1], prices[i]+dp[i-1][0])
+		dp[i][0] = max(dp[i-1][0], dp[i-1][1]-prices[i]) // 前一天持有股票时现金,前一天卖掉股票并在今天买入股票时的现金
+		dp[i][1] = max(dp[i-1][1], dp[i-1][0]+prices[i]) // 前一天不持有股票时现金,前一天持有股票并在今天卖掉的现金
 	}
 
 	return dp[len(prices)-1][1]
@@ -27,5 +27,6 @@ func max(a, b int) int {
 	if a > b {
 		return a
 	}
+
 	return b
 }
